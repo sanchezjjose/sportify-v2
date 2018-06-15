c15b4433b8f463f4fb3c010759efd3dda835b7e0
Jose Sanchez <sanchezjjose@gmail.com>
2018-06-14 22:46:39 -0400

    break up app into components

--- src/App.js
+++ src/App.js
@@ -20,58 +20,77 @@ class App extends Component {
 
   render() {
     return (
-      <div className='App'>
-        
-        {/* NavComponent */}
-        <nav className='App-navigation'>
-          <div className='App-logo'>sportify</div>
-          <div className='nav-items'>
-            <button className='nav-home'>
-              <img src={homeNavIcon} className="tab-icon" alt="home" />
-              <span className='nav-text'> home </span>
-            </button>
-            <button className='nav-schedule'>
-              <img src={scheduleNavIcon} className="tab-icon" alt="schedule" />
-              <span className='nav-text'> schedule </span>
-            </button>
-          </div>
-        </nav>
-        
-        {/* HeaderComponent */}
-        <header className='App-header'>
-          <div className='header-title'>
-            Pickup Game
-          </div>
-        </header>
-
+      <div className='App'> 
+        <NavComponent />
+        <HeaderComponent title='Pickup Game' />
         <div className='container'>
-
-          {/* DetailsComponent */}
-          <div className='content'>
-            <div className='game-date'>{this.state.schedule[0].date}</div>
-            <div className='game-location-name'>{this.state.schedule[0].location}</div>
-            <div className='game-location-address'>{this.state.schedule[0].address}</div>
-
-            {/* RsvpComponent */}
-            <button className='rsvp-button'>IN</button>
-            <button className='rsvp-button'>OUT</button>
-          </div>
-
+          <DetailsComponent schedule={this.state.schedule[0]} />
           <div className='line-divider'></div>
-
-          {/* RosterComponent */}
-          <div className='roster'>
-            <div className='roster-title'>Roster</div>
-            <div className='roster-subtitle'>{this.state.players.rsvpYes.length} player(s) confirmed</div>
-            
-            {this.state.players.rsvpYes.map (name => {
-              return <div key={name} className='roster-rsvp-in'>{name}</div>;
-            })}
-          </div>
+          <RosterComponent players={this.state.players} />
         </div>
       </div>
     );
   }
+}
+
+const NavComponent = () => {
+	return (
+		<nav className='App-navigation'>
+      <div className='App-logo'>sportify</div>
+      <div className='nav-items'>
+        <button className='nav-home'>
+          <img src={homeNavIcon} className="tab-icon" alt="home" />
+          <span className='nav-text'> home </span>
+        </button>
+        <button className='nav-schedule'>
+          <img src={scheduleNavIcon} className="tab-icon" alt="schedule" />
+          <span className='nav-text'> schedule </span>
+        </button>
+      </div>
+    </nav>
+	);
+}
+
+const HeaderComponent = ({ title }) => {
+	return (
+		<header className='App-header'>
+      <div className='header-title'>
+        {title}
+      </div>
+    </header>
+	);
+}
+
+const DetailsComponent = ({ schedule }) => {
+	return (
+		<div className='content'>
+	    <div className='game-date'>{schedule.date}</div>
+	    <div className='game-location-name'>{schedule.location}</div>
+	    <div className='game-location-address'>{schedule.address}</div>
+
+	    <RsvpComponent textValue='IN' />
+	    <RsvpComponent textValue='OUT' />
+	  </div>
+  )
+}
+
+const RsvpComponent = ({ textValue }) => {
+	return (
+		<button className='rsvp-button'>{textValue}</button>
+	);
+}
+
+const RosterComponent = ({ players }) => {
+	return (
+		<div className='roster'>
+      <div className='roster-title'>Roster</div>
+      <div className='roster-subtitle'>{players.rsvpYes.length} player(s) confirmed</div>
+      
+      {players.rsvpYes.map (name => {
+        return <div key={name} className='roster-rsvp-in'>{name}</div>;
+      })}
+    </div>
+	);
 }
 
 export default App;
