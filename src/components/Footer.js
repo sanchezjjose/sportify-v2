3cb91d6111319f36ce77a9ee270de77cc5c18738
josanche <jose.sanchez@bamtechmedia.com>
2018-09-16 21:36:34 -0400

    Fix Link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import homeNavIcon from './home-nav-icon.svg';
 import scheduleNavIcon from './schedule-nav-icon.svg';
