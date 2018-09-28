fa79c73b149d800d261a885c8cd014c65c12f498
josanche <jose.sanchez@bamtechmedia.com>
2018-09-28 09:37:17 -0400

    Convert Home component to class

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../Header/Header';
 import Details from '../Details/Details';
 import Roster from '../Roster/Roster';
 import Navigation from '../Navigation/Navigation';
+
 import './Home.css';
 
-const Home = ({ schedule, players }) => {
-  const nextGame = (schedule.length > 0 && [].concat(schedule[0])) || [];
+class Home extends Component {
 
-  return (
-    <div className='Home'>
-      <Navigation />
-      <Header title='Pickup Game' />
-      <div className='content-wrapper'>
-        <div className='content'>
-          <div className='next-game-details-wrapper'>
-            {nextGame.map(game => 
-              <Details key={game.date} game={game} />
-            )}
+  componentDidMount () {
+    console.log('Home component mounted...');
+  }
 
-            <div className='line-divider'></div>
+  render () {
+    // TODO: this should be a single object
+    const nextGame = (this.props.schedule.length > 0 && [].concat(this.props.schedule[0])) || [];
 
-            {players.length > 0 &&
-              <Roster players={players} />
-            }
+    return (
+      <div className='Home'>
+        <Navigation />
+        <Header title='Pickup Game' />
+        <div className='content-wrapper'>
+          <div className='content'>
+            <div className='next-game-details-wrapper'>
+              {nextGame.map(game => 
+                <Details key={game.date} game={game} />
+              )}
+
+              <div className='line-divider'></div>
+
+              {this.props.players.length > 0 &&
+                <Roster players={this.props.players} />
+              }
+            </div>
           </div>
         </div>
       </div>
-    </div>
     );
+  }
 }
 
 export default Home;
