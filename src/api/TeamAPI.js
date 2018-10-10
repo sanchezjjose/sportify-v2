bcedaf5154aad68cef28ad01f042b253aefc1cba
josanche <jose.sanchez@bamtechmedia.com>
2018-10-10 11:04:44 -0400

    Add to TeamAPI

--- src/api/TeamAPI.js
+++ src/api/TeamAPI.js
@@ -37,6 +37,41 @@ const getTeam = (id) => {
   });
 };
 
+const updateGame = (teamId, seasonId, game) => {
+  console.log(`Updating game ${game.id}...`);
+
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: 'Teams',
+      Key: {
+        'id': teamId
+      },
+      UpdateExpression: `SET seasons.#s.schedule.#g = :game`,
+      ExpressionAttributeNames: {
+        "#s": seasonId,
+        "#g": game.id
+      },
+      ExpressionAttributeValues: {
+          ":game": game,
+      },
+      ReturnValues:"ALL_NEW"
+    };
+
+    docClient.update(params, (err, data) => {
+      if (err) {
+        console.error('Unable to add game. Error JSON:', JSON.stringify(err, null, 2));
+        reject(err);
+
+      } else {
+        const teams = JSON.parse(JSON.stringify(data, null, 2));
+        const team = (teams && teams.Item) || {};
+
+        resolve(team);
+      }
+    })
+  });
+};
+
 const addPlayer = (teamId, seasonId, gameId, playerName) => {
   console.log(`Adding ${playerName} to game...`);
 
@@ -73,8 +108,107 @@ const addPlayer = (teamId, seasonId, gameId, playerName) => {
   });
 };
 
-const removePlayer = (id, playerName) => {
-  console.log(`Removing ${playerName} from game...`);
+const removePlayer = (teamId, seasonId, gameId, updatedPlayers) => {
+  console.log(`Removing player from game...`);
+
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: 'Teams',
+      Key: {
+        'id': teamId
+      },
+      UpdateExpression: `SET seasons.#s.schedule.#g.#p = :players`,
+      ExpressionAttributeNames: {
+        "#p": "players",
+        "#s": seasonId,
+        "#g": gameId
+      },
+      ExpressionAttributeValues: {
+          ":players": updatedPlayers
+      },
+      ReturnValues:"ALL_NEW"
+    };
+
+    docClient.update(params, (err, data) => {
+      if (err) {
+        console.error('Unable to remove player. Error JSON:', JSON.stringify(err, null, 2));
+        reject(err);
+
+      } else {
+        const teams = JSON.parse(JSON.stringify(data, null, 2));
+        const team = (teams && teams.Item) || {};
+
+        resolve(team);
+      }
+    })
+  });
 };
 
-export { getTeam, addPlayer, removePlayer };
+const addGame = (teamId, seasonId, game) => {
+  console.log(`Adding game to season...`);
+
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: 'Teams',
+      Key: {
+        'id': teamId
+      },
+      UpdateExpression: `SET seasons.#s.schedule.#g = :game`,
+      ExpressionAttributeNames: {
+        "#s": seasonId,
+        "#g": game.id
+      },
+      ExpressionAttributeValues: {
+          ":game": game,
+      },
+      ReturnValues:"ALL_NEW"
+    };
+
+    docClient.update(params, (err, data) => {
+      if (err) {
+        console.error('Unable to add game. Error JSON:', JSON.stringify(err, null, 2));
+        reject(err);
+
+      } else {
+        const teams = JSON.parse(JSON.stringify(data, null, 2));
+        const team = (teams && teams.Item) || {};
+
+        resolve(team);
+      }
+    })
+  });
+};
+
+const removeGame = (teamId, seasonId, gameId) => {
+  console.log(`Removing game ${gameId}...`);
+
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: 'Teams',
+      Key: {
+        'id': teamId
+      },
+      UpdateExpression: `REMOVE seasons.#s.schedule.#g`,
+      ExpressionAttributeNames: {
+        "#s": seasonId,
+        "#g": gameId
+      },
+      ReturnValues:"ALL_NEW"
+    };
+
+    docClient.update(params, (err, data) => {
+      if (err) {
+        console.error('Unable to remove game. Error JSON:', JSON.stringify(err, null, 2));
+        reject(err);
+
+      } else {
+        const teams = JSON.parse(JSON.stringify(data, null, 2));
+        const team = (teams && teams.Item) || {};
+
+        resolve(team);
+      }
+    })
+  });
+};
+
+export { getTeam, addPlayer, removePlayer, updateGame, addGame, removeGame };
