82f90d744bb692d66a4327cc3643837b27917ee6
Jose Sanchez <sanchezjjose@gmail.com>
2018-10-21 19:02:10 -0400

    Form button

--- src/components/Roster/Roster.js
+++ src/components/Roster/Roster.js
@@ -9,28 +9,36 @@ class Roster extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      showAddPlayerBtn: false
+    };
+
     this.addPlayer = this.addPlayer.bind(this);
     this.removePlayer = this.removePlayer.bind(this);
+    this.shouldShowButton = this.shouldShowButton.bind(this);
   }
 
   addPlayer(e, metadata) {
-    if (e.keyCode === 13) {
-      e.preventDefault();
+    e.preventDefault();
 
-      const name = e.target.value;
-      const newRoster = this.props.roster.concat(name);
+    const selector = e.target.querySelector('input');
+    const name = selector.value;
+    const newRoster = this.props.roster.concat(name);
 
-      this.props.handleRosterChange(newRoster, this.props.gameIndex);
+    this.props.handleRosterChange(newRoster, this.props.gameIndex);
 
-      TeamAPI.addPlayer(
-        metadata.teamId,
-        metadata.seasonId,
-        this.props.gameId,
-        name
-      );
+    TeamAPI.addPlayer(
+      metadata.teamId,
+      metadata.seasonId,
+      this.props.gameId,
+      name
+    );
 
-      e.target.value = '';
-    }
+    selector.value = '';
+
+    this.setState({ 
+      showAddPlayerBtn: false
+    });
   }
 
   removePlayer(e, name, metadata) {
@@ -46,6 +54,12 @@ class Roster extends Component {
     );
   }
 
+  shouldShowButton(e) {
+    this.setState({ 
+      showAddPlayerBtn: e.target.value.length > 0 
+    });
+  }
+
   render() {
     const roster = this.props.roster || [];
 
@@ -56,9 +70,14 @@ class Roster extends Component {
             <div className='roster-title'>Roster</div>
             <div className='roster-subtitle'>{roster.length} player(s) confirmed</div>
             <div className='rsvp-form'>
-              <form>
-                <input type='text' className='name-field' onKeyDown={(e) => this.addPlayer(e, metadata)} placeholder='Enter Player Name' />
-                <input type='submit' className='submit-button' />
+              <form onSubmit={e => this.addPlayer(e, metadata)}>
+                <input type='text' onChange={this.shouldShowButton} placeholder='Enter Player Name' className='name-field' />
+                {this.state.showAddPlayerBtn && 
+                  <a onClick={this.form.submit} className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">add</i></a>
+                  // <button className='btn waves-effect waves-light' type='submit' name='action'>(+)
+                    // { <i className='add-player-button material-icons'>arrow_back_ios</i> }
+                  // </button>
+                }
               </form>
             </div>
             <div className='roster-rsvp-in-container'>
