fb541054fcd6172cd8bd975aace0bef93f3c0bbb
Jose Sanchez <sanchezjjose@gmail.com>
2018-10-29 07:02:12 -0400

    Fix whitespace on landing

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -9,8 +9,8 @@ const Landing = () => {
       <div className='content'>
         <h2 className='welcome-message'><span>Welcome to Sportify.</span></h2>
         <p className='description'>
-          {`
-              Create a team, schedule games, build a rosters, and start playing!
+          {
+            `Create a team, schedule games, build a rosters, and start playing!
 
               With Sportify, everyone can RSVP to games, so you always know if you have enough players.
 
@@ -20,8 +20,8 @@ const Landing = () => {
               - Pick who will bring the game ball.
               - Get directions to game location.
 
-              Please visit your team page to get started.
-          `}
+              Please visit your team page to get started.`
+          }
         </p>
       </div>
     </div>
