f33c0335a59e8299fe40211a8a08dc1f054bb69d
josanche <jose.sanchez@bamtechmedia.com>
2018-11-07 22:35:15 -0500

    Fix schedule page styles

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -9,18 +9,20 @@ const Schedule = ({ schedule }) => {
       <Navigation />
       <Header title='Schedule' />
       <div className='content'>
-        {schedule.map(game => {
-          return (
-            <div key={game.id} className='game-details-wrapper'>
-              <div className='game-details'>
-                <div className='game-date'>{game.date}</div>
-                <div className='game-location-name'>{game.location}</div>
-                <div className='game-location-address'>{game.address}</div>
+        <div className='content-wrapper'>
+          {schedule.map(game => {
+            return (
+              <div key={game.id} className='game-details-wrapper'>
+                <div className='game-details'>
+                  <div className='game-date'>{game.date}</div>
+                  <div className='game-location-name'>{game.location}</div>
+                  <div className='game-location-address'>{game.address}</div>
+                </div>
+                <div className='line-divider'></div>
               </div>
-              <div className='line-divider'></div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
